4d956a9e19b521ac30dd9bc1287598c6b3a7e337
alemoles <alemolerio@gmail.com>
2024-04-07 14:56:09 -0300

    Android Kotlin OpenAi | create config handler for open ai

---
+++ app/src/main/java/com/devstromo/openaikotlin/ui/chat/core/config/Config.kt
@@ -0,0 +1,14 @@
+package com.devstromo.openaikotlin.ui.chat.core.config
+
+import com.aallam.openai.api.http.Timeout
+import com.aallam.openai.client.OpenAI
+import com.devstromo.openaikotlin.BuildConfig
+import kotlin.time.Duration.Companion.seconds
+
+fun getOpenAiConfig(): OpenAI {
+    val apiKey = BuildConfig.OPENAI_API_KEY
+    return OpenAI(
+        token = apiKey,
+        timeout = Timeout(socket = 60.seconds)
+    )
+}
