175604acd855f5c618ce4db4817daef91a5d46f1
alemoles <alemolerio@gmail.com>
2024-04-08 21:00:18 -0300

    Android Kotlin OpenAi | code refactor

--- app/src/main/java/com/devstromo/openaikotlin/MainActivity.kt
+++ app/src/main/java/com/devstromo/openaikotlin/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.devstromo.openaikotlin.ui.chat.presentation.chat.ChatScreen
 import com.devstromo.openaikotlin.ui.theme.OpenAiKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +18,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             OpenAiKotlinTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ChatScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun AppPreview() {
     OpenAiKotlinTheme {
-        Greeting("Android")
+        ChatScreen()
     }
 }
--- app/src/main/java/com/devstromo/openaikotlin/ui/chat/presentation/chat/ChatScreen.kt
+++ app/src/main/java/com/devstromo/openaikotlin/ui/chat/presentation/chat/ChatScreen.kt
@@ -1,13 +1,23 @@
 package com.devstromo.openaikotlin.ui.chat.presentation.chat
 
+
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 
 @Composable
-fun ChatScreen() {
+fun ChatScreen(
+    modifier: Modifier = Modifier
+) {
     val prompt by remember {
         mutableStateOf("A brown fox on the ground")
     }
+
+    Text(
+        text = "Image Prompt $prompt!",
+        modifier = modifier
+    )
 }
