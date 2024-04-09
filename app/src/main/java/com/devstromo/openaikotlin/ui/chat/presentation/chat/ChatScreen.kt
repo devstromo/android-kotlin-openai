e90444b509dc7d83ab070089fb3051bc54c0a7ed
alemoles <alemolerio@gmail.com>
2024-04-08 21:20:54 -0300

    Android Kotlin OpenAi | create chat input component

--- app/src/main/java/com/devstromo/openaikotlin/ui/chat/presentation/chat/ChatScreen.kt
+++ app/src/main/java/com/devstromo/openaikotlin/ui/chat/presentation/chat/ChatScreen.kt
@@ -1,16 +1,25 @@
 package com.devstromo.openaikotlin.ui.chat.presentation.chat
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.devstromo.openaikotlin.ui.theme.OpenAiKotlinTheme
+import com.devstromo.openaikotlin.ui.theme.kLightGrey
 
 @Composable
 fun ChatScreen(
@@ -26,10 +35,34 @@ fun ChatScreen(
         Text(
             text = "Image Prompt $prompt!",
         )
+        ChatInput(
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(10.dp)
+        )
     }
 
 }
 
+
+@Composable
+fun ChatInput(
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .height(30.dp)
+            .fillMaxWidth()
+            .background(
+                color = kLightGrey,
+                shape = ShapeDefaults.Medium
+            )
+    ) {
+        Text(
+            text = "Input",
+        )
+    }
+}
 
 @Preview
 @Composable
