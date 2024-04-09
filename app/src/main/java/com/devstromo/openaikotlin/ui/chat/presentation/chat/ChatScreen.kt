7207498c31b7a39ed9d7f45ba52eb41c9a844a28
alemoles <alemolerio@gmail.com>
2024-04-08 21:10:10 -0300

    Android Kotlin OpenAi | add preview for chat screen

--- app/src/main/java/com/devstromo/openaikotlin/ui/chat/presentation/chat/ChatScreen.kt
+++ app/src/main/java/com/devstromo/openaikotlin/ui/chat/presentation/chat/ChatScreen.kt
@@ -2,12 +2,15 @@ package com.devstromo.openaikotlin.ui.chat.presentation.chat
 
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.devstromo.openaikotlin.ui.theme.OpenAiKotlinTheme
 
 @Composable
 fun ChatScreen(
@@ -22,6 +25,18 @@ fun ChatScreen(
     ) {
         Text(
             text = "Image Prompt $prompt!",
+        )
+    }
+
+}
+
+
+@Preview
+@Composable
+private fun ChatScreenPreview() {
+    OpenAiKotlinTheme {
+        ChatScreen(
+            modifier = Modifier.fillMaxSize()
         )
     }
 
