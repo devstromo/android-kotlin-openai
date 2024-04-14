5fd28b167eb52fe0387f2a9ca9c02ba0da3de5e7
alemoles <alemolerio@gmail.com>
2024-04-14 20:46:42 -0300

    Android Kotlin OpenAi | include test chat completion for open ai

--- app/src/main/java/com/devstromo/openaikotlin/chat/data/AndroidGptController.kt
+++ app/src/main/java/com/devstromo/openaikotlin/chat/data/AndroidGptController.kt
@@ -1,5 +1,11 @@
 package com.devstromo.openaikotlin.chat.data
 
+import android.util.Log
+import com.aallam.openai.api.chat.ChatCompletion
+import com.aallam.openai.api.chat.ChatCompletionRequest
+import com.aallam.openai.api.chat.ChatMessage
+import com.aallam.openai.api.chat.ChatRole
+import com.aallam.openai.api.model.ModelId
 import com.aallam.openai.client.OpenAI
 import com.devstromo.openaikotlin.chat.core.config.getOpenAiConfig
 import com.devstromo.openaikotlin.chat.domain.GptController
@@ -8,6 +14,20 @@ class AndroidGptController() : GptController {
     private val openAI: OpenAI by lazy { getOpenAiConfig() }
 
     override suspend fun sendMessage(message: String) {
-        TODO("Not yet implemented")
+        val chatCompletionRequest = ChatCompletionRequest(
+            model = ModelId("gpt-3.5-turbo"),
+            messages = listOf(
+                ChatMessage(
+                    role = ChatRole.System,
+                    content = "You are a helpful assistant!"
+                ),
+                ChatMessage(
+                    role = ChatRole.User,
+                    content = message
+                )
+            )
+        )
+        val completion: ChatCompletion = openAI.chatCompletion(chatCompletionRequest)
+        Log.i("Chat completion", " Completion message $completion")
     }
 }
