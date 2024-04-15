0a34f09af9a20ed2ccbf1e4a3bfb738158750b7c
alemoles <alemolerio@gmail.com>
2024-04-14 21:28:03 -0300

    Android Kotlin OpenAi | add send message function to view model

--- app/src/main/java/com/devstromo/openaikotlin/chat/presentation/chat/ChatGtpViewModel.kt
+++ app/src/main/java/com/devstromo/openaikotlin/chat/presentation/chat/ChatGtpViewModel.kt
@@ -1,9 +1,12 @@
 package com.devstromo.openaikotlin.chat.presentation.chat
 
 import androidx.lifecycle.ViewModel
+import com.devstromo.openaikotlin.chat.data.GPTMessage
 import com.devstromo.openaikotlin.chat.domain.GptController
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 
@@ -12,4 +15,13 @@ class ChatGtpViewModel @Inject constructor(
     private val gptController: GptController
 ) : ViewModel() {
     private val _state = MutableStateFlow(ChatUiState())
+    val state = _state.asStateFlow()
+
+    fun sendMessage(message: GPTMessage) {
+        _state.update {
+            it.copy(
+                messages = it.messages + message
+            )
+        }
+    }
 }
