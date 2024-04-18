712b95b4fd20ceaa5f8dda73d2ee789de5569162
alemoles <alemolerio@gmail.com>
2024-04-17 22:42:23 -0300

    Android Kotlin OpenAi | create type writer text component

---
+++ app/src/main/java/com/devstromo/openaikotlin/chat/presentation/chat/components/TypewriterText.kt
@@ -0,0 +1,66 @@
+package com.devstromo.openaikotlin.chat.presentation.chat.components
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
+import kotlin.streams.toList
+
+@Composable
+fun TypewriterText(
+    texts: List<String>,
+) {
+    var textIndex by remember {
+        mutableIntStateOf(0)
+    }
+    var textToDisplay by remember {
+        mutableStateOf("")
+    }
+    val textCharsList: List<List<String>> = remember {
+        texts.map {
+            it.splitToCodePoints()
+        }
+    }
+
+    LaunchedEffect(
+        key1 = texts,
+    ) {
+        while (textIndex < textCharsList.size) {
+            textCharsList[textIndex].forEachIndexed { charIndex, _ ->
+                textToDisplay = textCharsList[textIndex]
+                    .take(
+                        n = charIndex + 1,
+                    ).joinToString(
+                        separator = "",
+                    )
+                delay(160)
+            }
+            textIndex = (textIndex + 1) % texts.size
+            delay(1000)
+        }
+    }
+
+    Text(
+        text = textToDisplay,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+    )
+}
+
+@RequiresApi(Build.VERSION_CODES.N)
+fun String.splitToCodePoints(): List<String> {
+    return codePoints()
+        .toList()
+        .map {
+            String(Character.toChars(it))
+        }
+}
