da9e2cec63eafaea74e9670f5af2d95f109eb2b4
alemoles <alemolerio@gmail.com>
2024-05-30 20:54:56 -0300

    Android Kotlin OpenAi | add image url to GPTMessage

--- app/src/main/java/com/devstromo/openaikotlin/chat/data/GPTMessage.kt
+++ app/src/main/java/com/devstromo/openaikotlin/chat/data/GPTMessage.kt
@@ -1,10 +1,12 @@
 package com.devstromo.openaikotlin.chat.data
 
+import android.net.Uri
 import java.util.UUID
 
 data class GPTMessage(
     val id: String = UUID.randomUUID().toString(),
     val message: String,
-    val isFromChatGTP: Boolean
+    val isFromChatGTP: Boolean,
+    val uri: Uri? = null,
 )
 
